13d677ae269e8101b9d9380cce94c82a2982cc8c
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2023-05-23 16:53:56 +0200

    Latest build new style

--- tldrnewspaperapp/App.js
+++ tldrnewspaperapp/App.js
@@ -1,25 +1,50 @@
 // import React in our code
 import React, { useState, useEffect } from 'react';
-// import all the components we are going to use
-import { SafeAreaView, Text,RefreshControl, ActivityIndicator,  Image, StyleSheet, View, Linking, ScrollView } from 'react-native';
+import { SafeAreaView, Text,RefreshControl, ActivityIndicator,  Image, StyleSheet, View, Linking, ScrollView, ImageBackground } from 'react-native';
 import { List, DataTable } from 'react-native-paper';
+import FlipCard from 'react-native-flip-card'
+import { color } from 'react-native-elements/dist/helpers';
 
 
 const App = () => {
   const [dataSource, setDataSource] = useState([]);
+  const [dataRecent, setDataRecent] = useState([]);
+  const [frontpageSource, setfrontpageSource] = useState([]);
+  const [weatherSource, setWeatherSource] = useState([]);
   const [expanded, setExpanded] = React.useState(true);
   const handlePress = () => setExpanded(!expanded);
   const [loading, setLoading] = useState(true)
 
 
-  
-
   useEffect(() => {
-    fetch('https://tldrnewspaper.com/article/mobileapi/json')
+    fetch('https://tldrnewspaper.com/article/mobileapi/sources')
       .then((response) => response.json())
       .then((responseJson) => {
-        console.log('Data arrived');
         setDataSource(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+      fetch('https://tldrnewspaper.com/article/mobileapi/recent')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setDataRecent(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+      fetch('https://tldrnewspaper.com/article/mobileapi/random')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setfrontpageSource(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+      fetch('https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b1b15e88fa797225412429c1c50c122a1')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setWeatherSource(responseJson);
         setLoading(false);
       })
       .catch((error) => {
@@ -29,12 +54,27 @@ const App = () => {
 
    const onRefresh = React.useCallback(async () => {
     setLoading(true);
-    fetch('https://tldrnewspaper.com/article/mobileapi/json')
+    fetch('https://tldrnewspaper.com/article/mobileapi/sources')
       .then((response) => response.json())
       .then((responseJson) => {
-        console.log('Data refreshed');
         setDataSource(responseJson);
-        setTimeout(() => {setLoading(false)}, 2000);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+      fetch('https://tldrnewspaper.com/article/mobileapi/recent')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setDataRecent(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+      fetch('http://localhost:8000/article/mobileapi/random')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setfrontpageSource(responseJson);
+        setLoading(false);
       })
       .catch((error) => {
         console.error(error);
@@ -47,57 +87,74 @@ const App = () => {
     return (
       // Flat List Item
       <View key={key}>
-        <Image style={styles.articleimage} source={{ uri: `${item.imageurl}` }} />
-        <Text style={styles.titlestyle} >
-          {item.title}
-        </Text>
-        <Text style={styles.datestyle} >
-          {item.pubdate}
-        </Text>
-        <List.Section>
-        <List.Accordion
-          theme={{colors: {background: 'white'}}}
-          title="TL:DR - AI Summary"
-          style={styles.articleheading}
-          expanded={expanded}
-          onPress={handlePress}>
-          <Text style={styles.bodystyle} >
-          {item.summary}
-        </Text>
-        </List.Accordion>
-        <List.Accordion
-          theme={{colors: {background: 'white'}}}
-          title="Differences between sources">
-          <Text style={styles.bodystyle} >
-            {item.comparison}
-          </Text>
-          <DataTable>
-            <DataTable.Header>
-              <DataTable.Title>Title</DataTable.Title>
-              <DataTable.Title >Source</DataTable.Title>
-            </DataTable.Header>
+        <FlipCard>
+          {/* Face Side */}
+          <View>
+          <ImageBackground style={styles.frontpagecard}resizeMode="cover" imageStyle={{ borderRadius: 6}} source={{ uri: `${item.imageurl}` }}>
+            <Text style={styles.headlinecardtext}>{item.title}</Text>
+            <ItemSeparatorView />
+          </ImageBackground>
+          </View >
+          {/* Back Side */}
+          <View style={{flex:1, width:"100%", backgroundColor:"#111827", borderRadius:20}}>
+            <ScrollView>
+            <Text style={styles.apptitledark}>{item.title}</Text>
+            <Text style={styles.datestyle} >
+              {item.pubdate}
+            </Text>
+            <Text style={styles.subtitlestyledark}>Summary</Text>
+            <Text style={styles.bodystyledark}>{item.summary}</Text>
+            <Image style={styles.frontpagecard}resizeMode="cover" imageStyle={{ borderRadius: 6}} source={{ uri: `${item.imageurl}` }}></Image>
+            <Text style={styles.subtitlestyledark}>Source comparison</Text>
+            <Text style={styles.bodystyledark} >{item.comparison}</Text>
+            <DataTable textStyle={{color:'white'}}>
+                <DataTable.Header>
+                  <DataTable.Title textStyle={{color:'white'}}>Source</DataTable.Title>
+                  <DataTable.Title textStyle={{color:'white'}}>Title</DataTable.Title>
+                </DataTable.Header>
 
-          {item.sources.map((item, key) => (
-            // key is the index of the array
-            // item is the single item of the array
+              {item.sources.map((item, key) => (
+                // key is the index of the array
+                // item is the single item of the array
 
-            <DataTable.Row key={key} onPress={ ()=>{ Linking.openURL(`${item.url}`)}}>
-            <DataTable.Cell>{item.title}</DataTable.Cell>
-            <DataTable.Cell>{item.sourcename}</DataTable.Cell>
-          </DataTable.Row>
-          ))}
-          </DataTable>
-        </List.Accordion>
-        <List.Accordion
-          theme={{colors: {background: 'white'}}}
-          title="Full Article">
-          <Text style={styles.bodystyle} >
-          {item.long_content}
-        </Text>
-        </List.Accordion>
-      </List.Section>
+                <DataTable.Row key={key} onPress={ ()=>{ Linking.openURL(`${item.url}`)}}>
+                <DataTable.Cell textStyle={{color:'white'}}>{item.sourcename}</DataTable.Cell>
+                <DataTable.Cell textStyle={{color:'white'}}>{item.title}</DataTable.Cell>
+              </DataTable.Row>
+              ))}
+              </DataTable>
+            <Text style={styles.subtitlestyledark}>Full article comparison</Text>
+            <Text style={styles.bodystyledark} >{item.long_content}</Text>
+            </ScrollView>
+          </View>
+        </FlipCard>
         <ItemSeparatorView />
       </View>
+    );
+  };
+
+  const CardView = (item, key) => {
+    return (
+      // Flat List Item
+      <View key={key} style={{maxHeight: 1200}}>
+        <FlipCard>
+          {/* Face Side */}
+          <View>
+            <ImageBackground style={styles.headlinecard}resizeMode="cover" imageStyle={{ borderRadius: 6}} source={{ uri: `${item.imageurl}` }}>
+              <Text style={styles.headlinecardtext}>{item.title}</Text>
+            </ImageBackground>
+          </View >
+          {/* Back Side */}
+            <View style={{height:1200, width:300, backgroundColor:"#111827", borderRadius:10}}>
+              <Text style={styles.subtitlestyledark}>TL:DR</Text>
+              <Text style={styles.bodystyledark }>{item.summary}</Text>
+            </View>
+        </FlipCard>
+      </View>
+      
+
+
+
     );
   };
 
@@ -111,39 +168,143 @@ const App = () => {
 
 
   return loading ? (
+    //if we're loading
     <SafeAreaView>
-      <View style={styles.header}>
-        <Text style={styles.apptitle}>Headlines</Text>
-        <Image style={styles.logo} source={require('./assets/logo.png')} />  
-      </View>
       <View style={styles.container}>
-        <Text style={styles.subtitlestyle}>The news in TL:DR format, written by AI </Text>
         <ActivityIndicator style={{paddingVertical:'50%'}} size="large" />
       </View>
+      <Image style={styles.logo} source={require('./assets/logo.png')} /> 
+      <Text style={{alignSelf:'center'}}>Loading more news...</Text>
+
     </SafeAreaView> ) : (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.header}>
-        <Text style={styles.apptitle}>Headlines</Text>
-        <Image style={styles.logo} source={require('./assets/logo.png')} />  
-      </View>
-      {ItemSeparatorView}
+      //If we're not loading
+    <View style={{ flex: 1 }}>
       <View style={styles.container}>
-        {/* List Item as a function */}
-        
-        <ScrollView
+      <ScrollView
         refreshControl={<RefreshControl refreshing={loading} onRefresh={onRefresh} />}>
-        <Text style={styles.subtitlestyle}>The news in TL:DR format, written by AI </Text>
+        <View style={{height:40}}></View>
+        <View style={{height:100, flexDirection:'row', paddingHorizontal:10}}>
+        <ImageBackground style={{flex:1, margin:4 }}resizeMode="cover" imageStyle={{ borderRadius: 10}} source={require('./assets/logo.png')}>
+        </ImageBackground>
+        <View style={{flex:2 , borderColor: 'lightgrey', borderWidth: 0, backgroundColor: 'white', margin:4, borderRadius:10, alignItems:'center', justifyContent:'center'}}>
+        <View style={{flexDirection:'row', alignItems:'center', justifyContent:'center'}}>
+        <Image
+            style={{width: 50, height: 50}}
+            source={{
+              uri: 'https://openweathermap.org/img/wn/'+String(weatherSource.weather[0].icon)+'@2x.png',
+            }}
+          />
+          <Text style={{fontWeight:'bold', fontSize:24}}>{Math.round(weatherSource.main.temp/10)} C°</Text>
+        </View>
+          <Text>{weatherSource.weather[0].description}</Text>
+          <Text>Windspeed: {weatherSource.wind.speed}</Text>
+        </View>
+      </View>
+      <Text style={styles.apptitle}>Front page</Text>
+
+
+      <FlipCard>
+      {/* Face Side */}
+      <View>
+      <ImageBackground style={styles.frontpagecard}resizeMode="cover" imageStyle={{ borderRadius: 6}} source={{ uri: `${frontpageSource[0].imageurl}` }}>
+        <Text style={styles.headlinecardtext}>{frontpageSource[0].title}</Text>
+        <ItemSeparatorView />
+      </ImageBackground>
+      </View >
+      {/* Back Side */}
+      <View style={{flex:1, width:"100%", backgroundColor:"#111827", borderRadius:20}}>
+        <ScrollView>
+        <Text style={styles.apptitledark}>{frontpageSource[0].title}</Text>
+        <Text style={styles.datestyle} >
+          {frontpageSource[0].pubdate}
+        </Text>
+        <Text style={styles.subtitlestyledark}>Summary</Text>
+        <Text style={styles.bodystyledark}>{frontpageSource[0].summary}</Text>
+        <Image style={styles.frontpagecard}resizeMode="cover" imageStyle={{ borderRadius: 6}} source={{ uri: `${frontpageSource[0].imageurl}` }}></Image>
+        <Text style={styles.subtitlestyledark}>Source comparison</Text>
+        <Text style={styles.bodystyledark} >{frontpageSource[0].comparison}</Text>
+        <DataTable textStyle={{color:'white'}}>
+            <DataTable.Header>
+              <DataTable.Title textStyle={{color:'white'}}>Source</DataTable.Title>
+              <DataTable.Title textStyle={{color:'white'}}>Title</DataTable.Title>
+            </DataTable.Header>
+
+          {frontpageSource[0].sources.map((item, key) => (
+            // key is the index of the array
+            // item is the single item of the array
+
+            <DataTable.Row key={key} onPress={ ()=>{ Linking.openURL(`${item.url}`)}}>
+            <DataTable.Cell textStyle={{color:'white'}}>{item.sourcename}</DataTable.Cell>
+            <DataTable.Cell textStyle={{color:'white'}}>{item.title}</DataTable.Cell>
+          </DataTable.Row>
+          ))}
+          </DataTable>
+        <Text style={styles.subtitlestyledark}>Full article comparison</Text>
+        <Text style={styles.bodystyledark} >{frontpageSource[0].long_content}</Text>
+        </ScrollView>
+      </View>
+    </FlipCard>
+      <Text style={styles.apptitle}>Summaries</Text>
+      <Text style={styles.subtitlestyle}>Just TL:DR</Text>
+
+
+      <View style={{height:250, flexDirection:'row', paddingLeft:10}}>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        {
+            //render all articles
+            dataSource.map(CardView)
+          }
+
+          
+        </ScrollView>
+      </View>
+      <Text style={styles.apptitle}>Latest</Text>
+      <Text style={styles.subtitlestyle}>Most recent</Text>
           {
             //render all articles
-            dataSource.map(ItemView)
+            dataRecent.map(ItemView)
           }
         </ScrollView>
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  frontpagecard: {
+    width:'100%', 
+    height:250, 
+    marginHorizontal:10, 
+    paddingHorizontal:10,
+    alignItems: "flex-end",
+    justifyContent: "flex-end"
+  },
+  headlinecard: {
+    width:250, 
+    height:200, 
+    backgroundColor: '#1A56DB', 
+    margin:4, 
+    borderRadius:10,
+    alignItems: "flex-end",
+    justifyContent: "flex-end"
+  },
+  headlinecardtext: {
+    margin:4, 
+    fontWeight:"bold",
+    fontSize: 20,
+    color: 'white',
+    backgroundColor: 'rgba(52, 52, 52, 0.4)',
+    borderRadius: 20,
+    alignSelf: 'center'
+    
+  },
+  headlinecardimage: {
+    flex: 1,
+    justifyContent: 'center',
+    borderRadius:20,
+    alignItems: "flex-end",
+    justifyContent: "flex-end"
+  },
   container: {
     backgroundColor: 'white',
   },
@@ -154,7 +315,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 0,
     margin: 5,
     overflow: 'hidden',
-    alignSelf: 'flex-start',
+    alignSelf: 'center',
   },
   header: {
     flexDirection: 'row',
@@ -170,10 +331,15 @@ const styles = StyleSheet.create({
     fontWeight: 'cursive',
   },
   apptitle: {
-    alignSelf: 'center',
     padding: 10,
-    fontSize: 42,
+    fontSize: 32,
     fontWeight: 'bold',
+  },
+  apptitledark: {
+    padding: 10,
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#fff'
   },
   titlestyle: {
     padding: 10,
@@ -185,10 +351,21 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
+  subtitlestyledark: {
+    padding: 10,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color:'white'
+  },
   bodystyle: {
     padding: 10,
     fontSize: 14,
 
+  },
+  bodystyledark: {
+    padding: 10,
+    fontSize: 14,
+    color:'white',
   },
   datestyle: {
     fontSize: 14,
