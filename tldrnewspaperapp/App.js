69bfdd75f8c2f4577c0d433edba6032f6283aaa5
Peter van Doorn <71013416+two-trick-pony-NL@users.noreply.github.com>
2023-05-23 23:36:27 +0200

    cleaned up some more

--- tldrnewspaperapp/App.js
+++ tldrnewspaperapp/App.js
@@ -34,7 +34,6 @@ const App = () => {
       <View style={styles.container}>
         <ActivityIndicator style={{paddingVertical:'50%'}} size="large" />
       </View>
-      <Image style={styles.logo} source={require('./assets/logo.png')} /> 
       <Text style={{alignSelf:'center'}}>Loading more news...</Text>
     </SafeAreaView> ) : (
       //If we're not loading
@@ -62,40 +61,6 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-  frontpagecard: {
-    width:'100%', 
-    height:250, 
-    marginHorizontal:10, 
-    paddingHorizontal:10,
-    alignItems: "flex-end",
-    justifyContent: "flex-end"
-  },
-  headlinecard: {
-    width:250, 
-    height:200, 
-    backgroundColor: '#1A56DB', 
-    margin:4, 
-    borderRadius:10,
-    alignItems: "flex-end",
-    justifyContent: "flex-end"
-  },
-  headlinecardtext: {
-    margin:4, 
-    fontWeight:"bold",
-    fontSize: 20,
-    color: 'white',
-    backgroundColor: 'rgba(52, 52, 52, 0.4)',
-    borderRadius: 20,
-    alignSelf: 'center'
-    
-  },
-  headlinecardimage: {
-    flex: 1,
-    justifyContent: 'center',
-    borderRadius:20,
-    alignItems: "flex-end",
-    justifyContent: "flex-end"
-  },
   container: {
     backgroundColor: 'white',
   },
@@ -114,23 +79,10 @@ const styles = StyleSheet.create({
     alignItems: 'flex-start'
     
   },
-  articleheading: {
-    color: '#000',
-    fontWeight: 'bold',
-  },
-  headertext: {
-    fontWeight: 'cursive',
-  },
   apptitle: {
     padding: 10,
     fontSize: 32,
     fontWeight: 'bold',
-  },
-  apptitledark: {
-    padding: 10,
-    fontSize: 32,
-    fontWeight: 'bold',
-    color: '#fff'
   },
   titlestyle: {
     padding: 10,
@@ -142,36 +94,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
-  subtitlestyledark: {
-    padding: 10,
-    fontSize: 18,
-    fontWeight: 'bold',
-    color:'white'
-  },
-  bodystyle: {
-    padding: 10,
-    fontSize: 14,
-
-  },
-  bodystyledark: {
-    padding: 10,
-    fontSize: 14,
-    color:'white',
-  },
-  datestyle: {
-    fontSize: 14,
-    color: '#D3D3D3',
-    paddingLeft: 10,
-  },
-  articleimage: {
-    resizeMode: 'cover',
-    padding: 20,
-    margin: 10,
-    height: 200,
-    width: '95%',
-    alignSelf: 'center',
-    borderRadius: 8,
-  }
 });
 
 export default App;
--- tldrnewspaperapp/components/cat.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Text} from 'react-native';
-
-const Header = () => {
-    return <Text>Hello, I am your cat!</Text>;
-};
-
-export default Header;
